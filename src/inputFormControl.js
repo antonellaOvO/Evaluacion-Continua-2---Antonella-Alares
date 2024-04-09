20117c449360d3b4dcbbc03e8b7853522d36ed47
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2024-04-09 21:29:03 +0200

    added second form -control y seguimiento- page and its JS code

--- src/inputFormControl.js
+++ src/inputFormControl.js
@@ -1,20 +1,25 @@
 let formTwo = document.getElementById("formTwo");
 
-function redireccionarAgain(event) {
+function redireccionar(event) {
   event.preventDefault();
 
-  let inputObjetivos = document.getElementById("objetivos").value;
-  let inputFrecuencia = document.getElementById("frecuencia").value;
-  let inputNombre = document.getElementById("nombre").value;
-  let inputEdad = document.getElementById("edad").value;
+  let inputEjercicioOne = document.getElementById("ejercicioOne").value;
+  let inputEjercicioTwo = document.getElementById("ejercicioTwo").value;
+  let inputEjercicioThree = document.getElementById("ejercicioThree").value;
+  let inputRepeticionOne = document.getElementById("repeticionOne").value;
+  let inputRepeticionTwo = document.getElementById("repeticionTwo").value;
+  let inputRepeticionThree = document.getElementById("repeticionThree").value;
 
   // Construir URL parametros
   var urlParams = new URLSearchParams();
-  urlParams.append("objetivos", inputObjetivos);
-  urlParams.append("frecuencia", inputFrecuencia);
-  urlParams.append("nombre", inputNombre);
-  urlParams.append("edad", inputEdad);
+  urlParams.append("ejercicioOne", inputEjercicioOne);
+  urlParams.append("ejercicioTwo", inputEjercicioTwo);
+  urlParams.append("ejercicioThree", inputEjercicioThree);
+  urlParams.append("repeticionOne", inputRepeticionOne);
+  urlParams.append("repeticionTwo", inputRepeticionTwo);
+  urlParams.append("repeticionThree", inputRepeticionThree);
+
   // Redirect to the result page with URL parameters
   window.location.href = "feedback-control.html?" + urlParams.toString();
 }
-formTwo.addEventListener("submit", redireccionarAgain);
+formTwo.addEventListener("submit", redireccionar);
--- src/resultadoFormControl.js
+++ src/resultadoFormControl.js
@@ -1,143 +1,46 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Coger valores seleccionados
   var urlParams = new URLSearchParams(window.location.search);
-  var objetivos = urlParams.get("objetivos");
-  var frecuencia = urlParams.get("frecuencia");
-  var nombre = urlParams.get("nombre");
-  var edad = urlParams.get("edad");
-  // Determinar rutina basada en la combinación de objetivos y frecuencia
-  var rutina = [];
-  if (objetivos === "Perder Peso" && frecuencia === "1-2 días a la semana") {
-    rutina = ["Push-ups", "Squats"];
-  } else if (
-    objetivos === "Perder Peso" &&
-    frecuencia === "2-3 días a la semana"
-  ) {
-    rutina = ["Push-ups", "Squats"];
-  } else if (
-    objetivos === "Mejorar Salud digestiva" &&
-    frecuencia === "1-2 días a la semana"
-  ) {
-    rutina = ["Running", "Cycling"];
-  } else if (
-    objetivos === "Mejorar Salud digestiva" &&
-    frecuencia === "2-3 días a la semana"
-  ) {
-    rutina = ["Running", "Cycling"];
-  } else if (
-    objetivos === "Mejorar Resistencia Cardiovascular" &&
-    frecuencia === "1-2 días a la semana"
-  ) {
-    rutina = ["Swimming", "Jumping rope", "HIIT workouts"];
-  } else if (
-    objetivos === "Mejorar Resistencia Cardiovascular" &&
-    frecuencia === "2-3 días a la semana"
-  ) {
-    rutina = ["Swimming", "Jumping rope", "HIIT workouts"];
-  } else if (
-    objetivos === "Mejorar Glúteos" &&
-    frecuencia === "1-2 días a la semana"
-  ) {
-    rutina = ["Push-ups", "Squats"];
-  } else if (
-    objetivos === "Mejorar Glúteos" &&
-    frecuencia === "2-3 días a la semana"
-  ) {
-    rutina = ["Push-ups", "Squats"];
-  } else if (
-    objetivos === "Aumentar Fuerza Muscular" &&
-    frecuencia === "1-2 días a la semana"
-  ) {
-    rutina = ["Swimming", "Jumping rope", "HIIT workouts"];
-  } else if (
-    objetivos === "Aumentar Fuerza Muscular" &&
-    frecuencia === "2-3 días a la semana"
-  ) {
-    rutina = ["Swimming", "Jumping rope", "HIIT workouts"];
-  } else if (
-    objetivos === "Marcar y Definir" &&
-    frecuencia === "1-2 días a la semana"
-  ) {
-    rutina = ["Push-ups", "Squats"];
-  } else if (
-    objetivos === "Marcar y Definir" &&
-    frecuencia === "2-3 días a la semana"
-  ) {
-    rutina = ["Push-ups", "Squats"];
+
+  var ejercicioOne = urlParams.get("ejercicioOne");
+  var ejercicioTwo = urlParams.get("ejercicioTwo");
+  var ejercicioThree = urlParams.get("ejercicioThree");
+  var repeticionOne = urlParams.get("repeticionOne");
+  var repeticionTwo = urlParams.get("repeticionTwo");
+  var repeticionThree = urlParams.get("repeticionThree");
+  let introTwo = document.getElementById("intro-section-two");
+  let introThree = document.getElementById("intro-section-three");
+
+  /*switch (true) {
+    case repeticionOne > 1 || repeticionTwo > 1 || repeticionThree > 1:
+      introTwo.textContent = `EJERCICIO "${ejercicioOne}" lo has realizado en total ${repeticionThree} repeticiones. A la próxima harás más repeticiones seguro!! 😉`;
+      introThree.textContent = `Mira los ejercicios que hemos seleccionado para ti, teniendo en cuenta tu dedicación de ${repeticionTwo}. Ayúdate con videos para saber en detalle como realizar estos ejercicios`;
+      break;
+    default:
+      alert(error);
+  }*/
+
+  //Realizamos matriz para extraer el ejercicio y repeticion correcto en cada línea de feedback
+  let ejercicios = [
+    [ejercicioOne, repeticionOne],
+    [ejercicioTwo, repeticionTwo],
+    [ejercicioThree, repeticionThree],
+  ];
+
+  for (i = 0; i < ejercicios.length; i++) {
+    for (j = 0; j < ejercicios.length; j++) {
+      let ejercicio = ejercicios[i][j];
+      let repeticion = ejercicios[i][j + 1];
+      let newLineEjercicio = document.createElement("h3");
+      if (ejercicio != undefined && ejercicio != Number(ejercicio)) {
+        newLineEjercicio.textContent = `Ejercicio "${ejercicio}" lo has realizado en total ${repeticion} repeticiones. A la próxima harás más repeticiones seguro!! 😉`;
+        newLineEjercicio.setAttribute("class", "my-5 p-3 rounded text-white");
+        newLineEjercicio.style.backgroundColor = "indianred";
+        newLineEjercicio.style.fontFamily = "ABeeZee";
+        introTwo.appendChild(newLineEjercicio);
+
+        console.log("ejercicio:" + ejercicio + "repeticion:" + repeticion);
+      }
+    }
   }
-
-  if (nombre === nombre && edad === edad) {
-    let nombreUsuario = nombre;
-    /*Para que la primeta letra del nombre del usario se siempre en mayúscula*/
-    nombreUsuario =
-      nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase();
-    /*Crear una sección para dar bienvenida al usario a su rutina diseñada*/
-    let introOne = document.getElementById("intro-section-one");
-    let introTwo = document.getElementById("intro-section-two");
-    introOne.textContent =
-      "Bienvenid@ " + `${nombreUsuario} 🎉` + " a tu Rutina personalizada!!";
-    introTwo.textContent = `${edad} años es una buena edad para empezar 😉`;
-  }
-
-  if (frecuencia === frecuencia) {
-    /*Crear una sección para dar bienvenida al usario a su rutina diseñada*/
-
-    let introThree = document.getElementById("intro-section-three");
-    introThree.textContent = `Mira los ejercicios que hemos seleccionado para ti, teniendo en cuenta tu dedicación de ${frecuencia}. Ayúdate con videos para saber en detalle como realizar estos ejercicios`;
-  }
-
-  // Display rutinas
-  let rutinaList = document.getElementById("rutina-listado");
-  let rutinaTitle = document.getElementById("rutina-title");
-
-  /*Crear el titulo de la rutina en función del objetivo seleccionado */
-  rutinaTitle.textContent = "Rutina " + `${objetivos}`;
-
-  rutinaTitle.classList.add("text-center");
-  rutina.forEach(function (rutina) {
-    let rutinaItem = document.createElement("div");
-    let subtitle = document.createElement("h3");
-    let image = document.createElement("img");
-    let video = document.createElement("iframe");
-
-    /*Añadir atributos al elemeno h3 (subtitle)*/
-    subtitle.setAttribute("class", "my-5");
-
-    /*Añadir atributos al elemeno imagen*/
-    image.setAttribute("src", "images/rutina-img.png");
-    image.setAttribute("class", "img-fluid");
-
-    /*Añadir atributos al elemeno video*/
-    video.setAttribute("class", "my-5");
-    video.setAttribute(
-      "src",
-      "https://www.youtube.com/embed/9v3uFEAL7Ys?si=P6oBp-ubOx3friw2"
-    );
-    video.setAttribute("width", "560");
-    video.setAttribute("height", "315");
-    video.setAttribute("title", "YouTube video player");
-    video.setAttribute("frameborder", "0");
-    video.setAttribute(
-      "allow",
-      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    );
-    video.setAttribute("frameborder", "0");
-    video.setAttribute("referrerpolicy", "object-fit-contain");
-    video.setAttribute("autoplay", "strict-origin-when-cross-origin");
-    video.setAttribute("allowfullscreen", "");
-
-    /*Customizaar contenido y style cada elemento de rutina*/
-    subtitle.textContent = rutina;
-    rutinaItem.classList.add("p-3");
-    rutinaItem.classList.add("mx-3");
-    rutinaItem.classList.add("bg-warning-subtle");
-    rutinaItem.classList.add("text-warning-emphasis");
-    rutinaItem.classList.add("fw-bold");
-    rutinaItem.classList.add("text-center");
-    rutinaList.appendChild(rutinaItem);
-    /*Añadir a cada elmento de rutina un título y una imagen*/
-    rutinaItem.appendChild(subtitle);
-    rutinaItem.appendChild(image);
-    rutinaItem.appendChild(video);
-  });
 });
