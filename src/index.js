e0e0acac10c2da74582107597ba8cc252ce71dd8
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2024-04-10 15:17:21 +0200

    Js code for managing validation input -age- on the form -diseña tu rutina-

--- src/index.js
+++ src/index.js
@@ -4,18 +4,33 @@ let formOne = document.getElementById("formOne");
 function redireccionar(event) {
   event.preventDefault();
 
-  let inputObjetivos = document.getElementById("objetivos").value;
-  let inputFrecuencia = document.getElementById("frecuencia").value;
-  let inputNombre = document.getElementById("nombre").value;
-  let inputEdad = document.getElementById("edad").value;
+  /*Validar input de edad en el formulario para que sea un número*/
 
-  // Construir URL parametros
-  var urlParams = new URLSearchParams();
-  urlParams.append("objetivos", inputObjetivos);
-  urlParams.append("frecuencia", inputFrecuencia);
-  urlParams.append("nombre", inputNombre);
-  urlParams.append("edad", inputEdad);
-  // Redirect to the result page with URL parameters
-  window.location.href = "ejercicios.html?" + urlParams.toString();
+  function validateForm() {
+    let edad = document.getElementById("edad").value;
+    if (edad != Number(edad)) {
+      let errorMessage = document.getElementById("errorOne");
+      errorMessage.textContent = "El valor de edad debe contener sólo números";
+    } else if (edad < 13 || edad > 90) {
+      let errorMessage = document.getElementById("errorOne");
+      errorMessage.textContent =
+        "El valor de edad debe contener sólo números de 13 a 90 años";
+    } else {
+      let inputObjetivos = document.getElementById("objetivos").value;
+      let inputFrecuencia = document.getElementById("frecuencia").value;
+      let inputNombre = document.getElementById("nombre").value;
+      let inputEdad = document.getElementById("edad").value;
+
+      // Construir URL parametros
+      var urlParams = new URLSearchParams();
+      urlParams.append("objetivos", inputObjetivos);
+      urlParams.append("frecuencia", inputFrecuencia);
+      urlParams.append("nombre", inputNombre);
+      urlParams.append("edad", inputEdad);
+      // Redirect to the result page with URL parameters
+      window.location.href = "ejercicios.html?" + urlParams.toString();
+    }
+  }
+  validateForm();
 }
 formOne.addEventListener("submit", redireccionar);
